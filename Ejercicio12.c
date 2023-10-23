e2003d9554eee4cc2473dac09d58d065972d80a3
YanouchJ <jeimy.yanouch@udla.edu.ec>
2023-10-22 21:55:17 -0500

    Implementación de dos ciclos WHILE para validar que el número sea positivo y de tres dígitos

--- Ejercicio12.c
+++ Ejercicio12.c
@@ -3,10 +3,23 @@
 int main(int argc, char const *argv[])
 {
     int num;
-    
+
     printf("12. Leer un número entero de tres dígitos y determinar si tiene el dígito 1. \n\n ");
     printf("Ingrese un número entero de tres dígitos: ");
     scanf("%d", &num);
 
+    while (num<=0)
+    {
+       printf("El número ingresado debe ser un entero positivo. \n");
+      printf("Ingrese un número entero de tres dígitos: ");
+      scanf("%d", &num);
+    }
+    
+    while (num<=100 || num>=999)
+  {
+     printf("El número ingresado debe ser de 3 dígitos. \n");
+    printf("Ingrese un número entero de tres dígitos: ");
+    scanf("%d", &num);
+  }
     return 0;
 }
