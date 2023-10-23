64aa850aff10f26a83ef43ab8a656c58deac41fd
YanouchJ <jeimy.yanouch@udla.edu.ec>
2023-10-22 23:55:03 -0500

    Ingreso de comentarios en el código

--- Ejercicio30.c
+++ Ejercicio30.c
@@ -2,20 +2,24 @@
 
 int main(int argc, char const *argv[])
 {
+    /*Declaración de variables*/
     int num;
+    /*Descripción del ejercicio*/
     printf("30. Leer un número entero y mostrar todos sus componentes numéricos, o sea, aquellos para quienes él sea un múltiplo. \n\n");
     printf("Ingrese un número entero: ");
     scanf("%d", &num);
 
+    /*Validación número mayor a cero */
     while (num<=0)
     {
         printf("El número ingresado debe ser mayor a cero. \n ");
         printf("Ingrese un número entero: ");
         scanf("%d", &num);
     }
-     
+        /*Impresión final*/
         printf("Los componentes numéricos múltiplos de %d son: ", num);
-        
+
+    /*Iteración hasta el número ingresado*/
      for (int i = 1; i <= num; i++)
      {
         if (num % i == 0)
