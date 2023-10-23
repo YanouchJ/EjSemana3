f07cee26740b34e6126bd0764a554fee92fba181
YanouchJ <jeimy.yanouch@udla.edu.ec>
2023-10-22 21:49:47 -0500

    Descripción del ejercicio y declaración de variables

---
+++ Ejercicio12.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int num;
+    
+    printf("12. Leer un número entero de tres dígitos y determinar si tiene el dígito 1. \n\n ");
+    printf("Ingrese un número entero de tres dígitos: ");
+    scanf("%d", &num);
+
+    return 0;
+}
