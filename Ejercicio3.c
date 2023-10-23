38e5fbf0e5583ab350382ea3b959b67a9d884614
YanouchJ <jeimy.yanouch@udla.edu.ec>
2023-10-22 21:23:39 -0500

    Ingreso de comentarios al código

--- Ejercicio3.c
+++ Ejercicio3.c
@@ -2,10 +2,13 @@
 
 int main(int argc, char const *argv[])
 {
+    /*Declaración e inicialización de variables*/
     int num, x=1;
+    printf("3. Leer un número entero y mostrar todos los divisores exactos del número comprendidos entre 1 y el número leído. \n");
     printf("Ingrese un numero entero: ");
     scanf("%d", &num);
 
+    /*Ciclo de validación de numero entero positivo*/
     while (num<=0)
       {
         printf("El numero ingresado debe ser positivo.\n ");
@@ -14,6 +17,8 @@ int main(int argc, char const *argv[])
       }
 
     printf("Divisores exactos de %d: \n", num);
+
+       /*Ciclo de validación de divisores exactos*/ 
         while (x<=num)
         {
             if (num % x == 0)
