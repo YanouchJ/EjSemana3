894a75efabd12eb42573698af38044be9e501c0c
YanouchJ <jeimy.yanouch@udla.edu.ec>
2023-10-22 22:33:46 -0500

    Declaración / Inicialización de variables y descripción del ejercicio

---
+++ Ejercicio21.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    printf("21. Leer un número entero y determinar a cuánto es igual la suma de sus dígitos. \n\n");
+  
+    int num;
+    int suma=0;
+    printf("Ingrese un numero entero: ");
+    scanf("%d", &num);
+
+    
+    return 0;
+}
