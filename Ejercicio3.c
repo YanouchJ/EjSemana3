73202ec3f1c118d9efc63efd032f79fe3bb5d028
YanouchJ <jeimy.yanouch@udla.edu.ec>
2023-10-22 21:19:02 -0500

    Implementación ciclo WHILE para validación de número entero positivo

--- Ejercicio3.c
+++ Ejercicio3.c
@@ -6,14 +6,21 @@ int main(int argc, char const *argv[])
     printf("Ingrese un numero entero: ");
     scanf("%d", &num);
 
-    printf("Divisores exactos de %d: \n", num);
-    while (x<=num)
+    while (num<=0)
       {
-        if (num % x == 0)
-          {
-          printf(" %d\n ", x);
-          }
-        x++;
+        printf("El numero ingresado debe ser positivo.\n ");
+        printf("Ingrese un numero entero: ");
+        scanf("%d", &num);
       }
+
+    printf("Divisores exactos de %d: \n", num);
+        while (x<=num)
+        {
+            if (num % x == 0)
+            {
+            printf(" %d\n ", x);
+            }
+            x++;
+        }
     return 0;
 }
