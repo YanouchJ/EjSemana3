fa399d7846f94ebede40cecaace39d0820db6dfe
YanouchJ <jeimy.yanouch@udla.edu.ec>
2023-10-22 23:47:43 -0500

    Implementación de ciclo WHILE para validar que el numero ingresado sea mayor a cero

--- Ejercicio30.c
+++ Ejercicio30.c
@@ -4,6 +4,15 @@ int main(int argc, char const *argv[])
 {
     int num;
     printf("30. Leer un número entero y mostrar todos sus componentes numéricos, o sea, aquellos para quienes él sea un múltiplo. \n\n");
+    printf("Ingrese un número entero: ");
+    scanf("%d", &num);
 
+    while (num<=0)
+    {
+        printf("El número ingresado debe ser mayor a cero. \n ");
+        printf("Ingrese un número entero: ");
+        scanf("%d", &num);
+    }
+    
     return 0;
 }
