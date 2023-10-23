a616682bb8576b34d6ca70b15b7f8a03baa70c8d
YanouchJ <jeimy.yanouch@udla.edu.ec>
2023-10-22 22:06:00 -0500

    Declaración e inicialización de variables para los dígitos ingresados e implementación de ciclo IF para validar si el número ingresado contiene 1

--- Ejercicio12.c
+++ Ejercicio12.c
@@ -14,12 +14,26 @@ int main(int argc, char const *argv[])
       printf("Ingrese un número entero de tres dígitos: ");
       scanf("%d", &num);
     }
-    
+
     while (num<=100 || num>=999)
   {
      printf("El número ingresado debe ser de 3 dígitos. \n");
     printf("Ingrese un número entero de tres dígitos: ");
     scanf("%d", &num);
   }
+
+    int D1 = num / 100;
+    int D2 = (num / 10) % 10;
+    int D3 = num % 10;
+
+        if (D1 == 1 || D2 == 1 || D3 == 1)
+        {
+            printf("El número %d tiene el dígito 1 en él.\n", num);
+        }
+        else
+        {
+            printf("El número %d no tiene el dígito 1 en él.\n", num);
+        }
+        
     return 0;
 }
