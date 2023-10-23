d5bf6c1c4ac7c4d4409362f7e15e6bcb804f6d88
YanouchJ <jeimy.yanouch@udla.edu.ec>
2023-10-22 23:43:23 -0500

    Descripción del ejercicio y declaración de variables

---
+++ Ejercicio30.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int num;
+    printf("30. Leer un número entero y mostrar todos sus componentes numéricos, o sea, aquellos para quienes él sea un múltiplo. \n\n");
+
+    return 0;
+}
