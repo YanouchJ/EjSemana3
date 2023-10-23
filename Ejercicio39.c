bf7b816c7a253e863b865ffef128567c205769f3
YanouchJ <jeimy.yanouch@udla.edu.ec>
2023-10-22 23:25:36 -0500

    Declaración e inicialización de variables + Descripción del ejercicio

---
+++ Ejercicio39.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int a=0, b=0, serie=0;
+
+    printf("39. Serie de FIBONACCI hasta llegar o sobrepasar el número 10000. \n\n ");
+    
+
+    return 0;
+}
