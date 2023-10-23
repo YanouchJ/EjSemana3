0cab729cc24ff6aa306b7d5e720f533506a94d01
YanouchJ <jeimy.yanouch@udla.edu.ec>
2023-10-22 23:32:43 -0500

    Ingreso de comentarios en el código y cambio del límite en ciclo WHILE de 10000 a 11000 para sobrepasar en uno el límite

--- Ejercicio39.c
+++ Ejercicio39.c
@@ -2,12 +2,15 @@
 
 int main(int argc, char const *argv[])
 {
+    /*Declaración e inicialización de variables*/
     int a=0, b=0, serie=0;
-
+    /*Descripción del ejercicio*/
     printf("39. Serie de FIBONACCI hasta llegar o sobrepasar el número 10000. \n\n ");
     
-    while (serie<=10000)
+    /*Validación de la impresión hasta el límite requerido*/
+    while (serie<=11000)
     {
+        /*Impresión de la serie*/
         printf("%d", serie);
         a=b;
         b=serie;
