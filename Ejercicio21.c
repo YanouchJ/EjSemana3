e6a4ea4b7b297830309b137a13f30523e4b3acba
YanouchJ <jeimy.yanouch@udla.edu.ec>
2023-10-22 23:00:29 -0500

    Implementación del ciclo WHILE para validar que el número ingresado sea positivo y ciclo FOR para uso de contador/acumulador y operaciones algebráicas

--- Ejercicio21.c
+++ Ejercicio21.c
@@ -4,18 +4,24 @@ int main(int argc, char const *argv[])
 {
     printf("21. Leer un número entero y determinar a cuánto es igual la suma de sus dígitos. \n\n");
   
-    int num;
-    int suma=0;
+    int num, suma, total;
+    
     printf("Ingrese un numero entero: ");
     scanf("%d", &num);
 
     while (num<=0)
     {
-        printf("El número ingresado debe ser un entero positivo. \n");
+        printf("El número ingresado debe ser un entero positivo. \n\n");
         printf("Ingrese un número entero: ");
         scanf("%d", &num);
     }
-
+       for (int i = 1; i < num; i++)
+       {
+        suma=suma+i;
+        total=suma+num;
+       }
+       
+    printf("Para %d, la suma de los dígitos es igual a: %d", num, total);
 
     return 0;
 }
