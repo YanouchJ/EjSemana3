36a293ca2a201bbe73aecfcc557bec47c5c43341
YanouchJ <jeimy.yanouch@udla.edu.ec>
2023-10-22 21:11:01 -0500

    Implementación ciclos WHILE e IF para validación de divisores exactos

--- Ejercicio3.c
+++ Ejercicio3.c
@@ -6,5 +6,14 @@ int main(int argc, char const *argv[])
     printf("Ingrese un numero entero: ");
     scanf("%d", &num);
 
+    printf("Divisores exactos de %d: \n", num);
+    while (x<=num)
+      {
+        if (num % x == 0)
+          {
+          printf(" %d\n ", x);
+          }
+        x++;
+      }
     return 0;
 }
